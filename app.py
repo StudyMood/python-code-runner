18e8e91d7e5068ca594dd91df3989a844bdba05d
Abhishek Kumar <studymood9988@gmail.com>
2026-01-05 19:09:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import subprocess
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 app = Flask(__name__)
 CORS(app)
@@ -45,3 +50,4 @@ def run_code():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
